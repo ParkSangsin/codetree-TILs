e5b3695ed49db058f61a382e807e2a25dd81f640
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-01 15:07:29 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240101/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 5;
+
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << a;
+    cout << "\n";
+    cout << b;
+
+    return 0;
+}
