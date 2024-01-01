7f289ddd32a4d7cfb68d94d44f392e80e6bb356e
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-01 13:55:06 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240101/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Total days in Year" << "\n" << "365" << "\n" << "Circumference rate" << "\n" << "3.1415926535";
+    return 0;
+}
