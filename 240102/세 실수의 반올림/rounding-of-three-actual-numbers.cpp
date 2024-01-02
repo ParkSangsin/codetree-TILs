87261b63409780c14fe188ee181260eda67c1373
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-02 17:04:06 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double a, b, c;
+
+	cin >> a >> b >> c;
+
+	cout << fixed;
+	cout.precision(3);
+	cout << a << "\n" << b << "\n" << c;
+	return 0;
+}
