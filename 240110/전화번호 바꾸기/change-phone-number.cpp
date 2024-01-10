81f2d36bd72a1b4b665b69cc5430a25c5d347f06
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-10 10:13:06 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+	int a, b, c;
+	char x;
+	cin >> a >> x >> b >> x >> c;
+	cout << "010" << x << c << x << b;
+
+	return 0;
+}
