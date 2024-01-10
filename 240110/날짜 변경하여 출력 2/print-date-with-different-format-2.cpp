428f8886b93656523cab695e516a7af4a3e5600f
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-10 10:06:30 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int mm, dd, yyyy;
+	char x;
+	cin >> mm >> x >> dd >> x >> yyyy;
+
+	cout << yyyy << '.' << mm << '.' << dd;
+
+	return 0;
+}
