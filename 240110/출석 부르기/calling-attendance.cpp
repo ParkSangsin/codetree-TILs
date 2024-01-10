13d3d301691bc4b5efce9b739f76b5f699b5f67d
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-10 13:10:02 +0900

    Title: 출석 부르기, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	
+	cin >> a;
+
+	if (a == 1) {
+		cout << "John";
+	}
+	else if (a == 2) {
+		cout << "Tom";
+	}
+	else if (a == 3) {
+		cout << "Paul";
+	}
+	return 0;
+}
