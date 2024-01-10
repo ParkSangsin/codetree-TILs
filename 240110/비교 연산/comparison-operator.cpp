0afee493d5b554d3bcf359ce28dbdbbf5733060d
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-10 13:48:12 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/비교 연산/comparison-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << (a >= b) << endl;
+	cout << (a > b) << endl;
+	cout << (b >= a) << endl;
+	cout << (b > a) << endl;
+	cout << (a == b) << endl;
+	cout << (a != b);
+
+	return 0;
+}
