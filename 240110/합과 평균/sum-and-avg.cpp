5152c00e8e15a43f650e3f477aacb2a8eee7695b
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-10 11:36:39 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a + b << " ";
+	cout << fixed;
+	cout.precision(1);
+	cout << double((a + b)) / 2;
+
+	return 0;
+
+}
