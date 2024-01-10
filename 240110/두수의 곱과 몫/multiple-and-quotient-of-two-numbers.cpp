92ed17f5424f453eeddaf528b8349d425be69e00
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-10 10:34:43 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << a << " * " << b << " = " << a * b;
+	cout << endl;
+	cout << a << " / " << b << " = " << a / b;
+
+	return 0;
+
+}
