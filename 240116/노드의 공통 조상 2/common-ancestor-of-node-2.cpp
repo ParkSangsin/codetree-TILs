a0448df911a3626070577495b49fe2052944791d
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-16 02:00:49 +0900

    Title: 노드의 공통 조상 2, Time: 42ms, Memory: 4MB, Status: Runtime Error - CodeTree

---
+++ 240116/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+
+#define MAX_N 50000
+#define MAX_H 16
+
+int n, root;
+int a, b; // 공통 조상을 구할 노드
+int q; // 질의 횟수
+bool visited[MAX_N];
+int par[MAX_H][MAX_N]; // par[i][j]: j번 노드에서 부모로 2^i번 이동 했을 때 조상 노드
+int depth[MAX_N]; // depth[i]: i번 노드의 깊이
+vector<int> edges[MAX_N];
+
+void dfs(int x) {
+	visited[x] = true;
+
+	for (int i = 0; i < edges[x].size(); i++) {
+		int y = edges[x][i];
+		
+		if (visited[y]) continue;
+
+		depth[y] = depth[x] + 1;
+		par[0][y] = x;
+
+		dfs(y);
+	}
+}
+
+int LCA(int a, int b) {
+	if (depth[a] < depth[b]) {
+		return LCA(b, a);
+	}
+
+	for (int h = MAX_H; h >= 0; h--) {
+		if (depth[a] - depth[b] >= (1 << h)) {
+			a = par[h][a];
+		}
+	}
+
+	if (a == b) return a;
+
+	for (int h = MAX_H; h >= 0; h--) {
+		if (par[h][a] != par[h][b]) {
+			a = par[h][a];
+			b = par[h][b];
+		}
+	}
+
+	return par[0][a];
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n - 1; i++) {
+		int p, c; // 부모 노드, 자식 노드
+		cin >> p >> c;
+		
+		edges[p].push_back(c);
+		edges[c].push_back(p);
+	}
+
+	depth[1] = 1;
+	dfs(1);
+
+	// par 채우기
+	for (int h = 1; h <= MAX_H; h++) {
+		for (int i = 1; i <= n; i++) {
+			par[h][i] = par[h - 1][par[h - 1][i]];
+		}
+	}
+
+	cin >> q;
+	while (q--) {
+		int a, b;
+		cin >> a >> b;
+		cout << LCA(a, b) << endl;
+	}
+	return 0;
+}
