3f7f84855843fa0a01775e83e1f8b36e11eaec00
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-17 01:36:43 +0900

    Title: 트리 완성, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240117/트리 완성/tree-completion.cpp
+++ 240117/트리 완성/tree-completion.cpp
@@ -1,49 +1,56 @@
-#include <iostream>
+static void union(int x, int y){
+        int X = find(x);
+        int Y = find(y);
 
-using namespace std;
+        uf[X] = Y; 
+    }
 
-#define MAX_N 100000
+    static int find(int x){
+        if(uf[x]==x){
+            return x;
+        }
+        return uf[x] = find(uf[x]);
+    }
 
-int n, m;
-char q;
-int a, b;
-int uf[MAX_N + 1];
-int cnt; // 사이클이 없을 때, 없애기 위해 자르는 연산이 필요한 횟수
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
 
-int find(int x) {
-	if (uf[x] == x) {
-		return x;
-	}
-	uf[x] = find(uf[x]);
-	return uf[x];
-}
+        Scanner sc = new Scanner(System.in); 
 
-void union_(int a, int b) {
-	a = find(a);
-	b = find(b);
-	uf[a] = b;
-}
+        n = sc.nextInt(); 
+        m = sc.nextInt();
+        uf = new int[n+1];
 
-int main() {
-	cin >> n >> m;
+        for(int i=1; i<=n; i++){
+            uf[i] = i;
+        }
 
-	// 유니온 파인드를 위한 초기 값 설정
-	for (int i = 1; i <= n; i++) {
-		uf[i] = i;
-	}
+        int countDelete = 0;
 
-	int res;
-	int flag = true;
-
-	for (int i = 1; i <= m; i++) {
-		cin >> a >> b;
-		// 두 점의 루트가 다르다면 (사이클이 생기지 않을 경우)
-		if (find(a) != find(b)) {
-			cnt++;
-			union_(a, b);
-		}	
-	}
-	// (n - 1) - cnt: 추가해야할 간선 수
-	// m - cnt는 제거해야할 간선 수 (사이클 때문)
-	cout << n - 1 - cnt + (m - cnt);
+        //끊는 연산 세기 
+            //합칠 때 이미 같은 그룹이면 끊어야함 
+        for(int  i=0; i<m; i++){
+            int x = sc.nextInt(); 
+            int y = sc.nextInt();
+            if(find(x)==find(y)){
+                countDelete++;
+                continue;
+            }
+            union(x, y);
+        }
+        //연결하는 연산 세기 
+            // 그룹이 다른 것의 갯수 세기 
+            // 이후 그룹 연결하기 
+        int prev = find(uf[1]);
+        int countPlus = 0;
+        for(int i=2; i<=n; i++){
+            if(find(uf[i-1])!=find(uf[i])){
+                prev = find(uf[i]);
+                union(i, i-1);
+                countPlus++;
+            }
+        }
+        System.out.println(countPlus+countDelete);
+        
+    }
 }
