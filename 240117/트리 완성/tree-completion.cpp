9588afe6d8fe2448a08e5101de8f23be23d8d0cb
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-17 01:34:51 +0900

    Title: 트리 완성, Time: 77ms, Memory: 0MB, Status: Passed - CodeTree

--- 240117/트리 완성/tree-completion.cpp
+++ 240117/트리 완성/tree-completion.cpp
@@ -8,7 +8,7 @@ int n, m;
 char q;
 int a, b;
 int uf[MAX_N + 1];
-int cnt; // 그래프 내 트리 개수
+int cnt; // 사이클이 없을 때, 없애기 위해 자르는 연산이 필요한 횟수
 
 int find(int x) {
 	if (uf[x] == x) {
@@ -37,10 +37,13 @@ int main() {
 
 	for (int i = 1; i <= m; i++) {
 		cin >> a >> b;
+		// 두 점의 루트가 다르다면 (사이클이 생기지 않을 경우)
 		if (find(a) != find(b)) {
 			cnt++;
 			union_(a, b);
 		}	
 	}
+	// (n - 1) - cnt: 추가해야할 간선 수
+	// m - cnt는 제거해야할 간선 수 (사이클 때문)
 	cout << n - 1 - cnt + (m - cnt);
 }
