fceebb981779f7f066a75b741a7d1b776575b9e9
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-22 14:26:13 +0900

    Title: 사이클 판별, Time: 7ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240122/사이클 판별/detect-cycle.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+#define MAX_N 100000
+
+int n, m; // 노드 개수
+vector<int> edges[MAX_N + 1];
+int indegree[MAX_N + 1]; // indegree[i]: i번 노드에 들어오는 간선 수
+bool visited[MAX_N + 1];
+
+queue<int> q;
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		edges[a].push_back(b);
+		indegree[b]++;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (indegree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	int cnt = 0;
+
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		cnt++;
+
+		for (int i = 0; i < edges[x].size(); i++) {
+			int y = edges[x][i];
+			indegree[y]--;
+			if (indegree[y] == 0) {
+				q.push(y);
+			}
+		}
+	}
+
+	if (cnt == n) cout << "Exists";
+	else cout << "Not Exists";
+
+	return 0;
+}
