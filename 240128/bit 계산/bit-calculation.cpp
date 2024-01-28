d7f9b182bf39106805dcc50cd75d2f76d75fb651
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-28 22:50:21 +0900

    Title: bit 계산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240128/bit 계산/bit-calculation.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+#define MAX_N 2000
+
+int q;
+
+int snum; // 집합 S: x가 존재한다면 2^x을 더해준다. (비트를 킴)
+
+int main() {
+	cin >> q;
+
+	for (int t = 0; t < q; t++) {
+		string str;
+		int x;
+		cin >> str >> x;
+
+		if (str == "add") {
+			snum |= (1 << x); // x번 비트를 켜라
+		}
+		else if (str == "delete") {
+			snum &= ~(1 << x); // x번 비트를 꺼라
+		}
+		else if (str == "print") {
+			if ((snum & (1 << x)) != 0) {
+				cout << 1 << "\n";
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+		else if (str == "toggle") {
+			if ((snum & (1 << x)) != 0) {
+				snum &= ~(1 << x); // x번 비트를 꺼라
+			}
+			else {
+				snum &= ~(1 << x); // x번 비트를 꺼라
+			}
+		}
+		else if (str == "clear") {
+			snum = 0;
+		}
+	}
+
+	return 0;
+}
