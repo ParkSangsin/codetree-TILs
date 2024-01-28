820a3fd06370965501d10889563434e83f6d9ec4
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-01-28 20:43:06 +0900

    Title: 가장 긴 좌우 대칭인 문자열 찾기 2, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240128/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+#define MAX_N 200020
+
+string temp, str;
+int n; // str의 크기
+
+int A[MAX_N];// A[i]: i인덱스를 중심으로 하는 펠린드롬 중 가장 긴 펠린드롬의 길이
+
+int cnt;
+
+int main() {
+	cin >> temp;
+
+	// 문자 사이에 #을 붙여 홀수 만들기
+	for (int i = 0; i < temp.size(); i++) {
+		str += "#";
+		str += temp[i];
+	}
+	str += "#";
+
+	n = str.size();
+
+	int r = -1, p = -1;
+
+	for (int i = 0; i < n; i++) {
+		if (r >= i) {
+			int ii = p - (i - p);
+			A[i] = min(r - i, A[ii]);
+		}
+
+		while (i - A[i] - 1 >= 0 && i + A[i] + 1 < n &&
+			str[i - A[i] - 1] == str[i + A[i] + 1]) {
+			A[i]++;
+		}
+
+		cnt += (A[i] + 1) / 2;
+
+		if (r < i + A[i]) {
+			r = i + A[i];
+			p = i;
+		}
+
+	}
+
+	cout << cnt / 2;
+
+	return 0;
+}
